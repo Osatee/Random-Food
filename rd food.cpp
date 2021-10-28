b736eb404c5ab2d458125764804c47322e111aef
Osatee Sukputanond <84953164+Osatee@users.noreply.github.com>
2021-10-28 21:34:21 +0700

    Update rd food.cpp

--- rd food.cpp
+++ rd food.cpp
@@ -5,8 +5,8 @@ int main()
 
 {
     char menu [50][5000] = {"Spaghetti","Sushi","Ramen","Krapow","Udon","Fried chicken with garlic","Sandwich",
-							"Hamburger","Omelette","Fried rice","Noodles","Pizza Company","KFC","Mcdonald","Shabu-Buffet",
-							"Texus","Steak","Papaya Salad","Fried egg","Tokbukki"};
+			    "Hamburger","Omelette","Fried rice","Noodles","Pizza Company","KFC","Mcdonald",
+			    "Shabu-Buffet","Texus","Steak","Papaya Salad","Fried egg","Tokbukki"};
     srand(time(NULL));
     int number = rand() % 20;
     printf("%s", menu[number]);
